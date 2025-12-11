e9b42bac51b620711ff25ae0e80e33e1b4ad9f0b
serega <serega@local.tuf>
2025-12-11 17:56:37 +0200

    🤳👊

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,6 +6,32 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string|null $description
+ * @property numeric $price
+ * @property string|null $image
+ * @property int|null $group_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Group|null $group
+ * @method static \Database\Factories\ItemFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereGroupId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Item whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Item extends Model
 {
     use HasFactory;
